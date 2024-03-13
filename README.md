0dcbcded7f89a353fd68449dd5dce81af0842026
stevechan <chanshunli@gmail.com>
2024-03-13 13:57:28 +0800

    [x] Go版本： 第一个实用的go写的zshrc笔记解析为markdown大纲 zshrc2md.go

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 ---
 
 - [ ] 写一个zshrc记录笔记解析为大纲markdown的脚本
-  - [x] Clojure版本已经完成: parse_zshrc_note_to_md_outline.clj (lambda化快速演算) => bb 版本：`./zshrc2md.clj`
-  - [ ] Go版本
+  - [x] Clojure版本已经完成: parse_zshrc_note_to_md_outline_clj (lambda化快速演算) => bb 版本：`./zshrc2md_clj`
+  - [x] Go版本： 第一个实用的go写的zshrc笔记解析为markdown大纲 zshrc2md.go
 
 格式如：
 ```bash
 ## aaa
 ## aaabbb => bbb => ccc
-## ee => fff 
+## ee => fff
 ```
 =>
 
@@ -23,4 +23,3 @@
 - ee
     - fff
 ```
-
--- zshrc2md.go
+++ zshrc2md.go
@@ -1,80 +1,37 @@
 package main
-
+// 第一个实用的go写的zshrc笔记解析为markdown大纲
 import (
 	"fmt"
 	"strings"
+	"regexp"
+	"io/ioutil"
 )
 
-func mapStr(arr []string, fn func(it string) string) []string {
-	var newArray = []string{}
-	for _, it := range arr {
-		newArray = append(newArray, fn(it))
-	}
-	return newArray
-}
+func repeatString(input string, times int) string {
+	var result strings.Builder
 
-func parseToMarkdown(input string) string {
-	lines := strings.Split(input, "\n")
-	var output strings.Builder
-
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if strings.HasPrefix(line, "# ") {
-			output.WriteString(fmt.Sprintf("#%s\n\n", line[2:]))
-		} else if strings.HasPrefix(line, "## ") {
-			output.WriteString(fmt.Sprintf("##%s\n\n", line[3:]))
-		} else if strings.HasPrefix(line, "* ") {
-			output.WriteString(fmt.Sprintf("*%s\n", line[2:]))
-		} else {
-			output.WriteString(fmt.Sprintf("%s\n", line))
-		}
+	for i := 0; i < times; i++ {
+		result.WriteString(input)
 	}
 
-	return output.String()
+	return result.String()
 }
 
 func main() {
-	//	var input string
-	//	fmt.Println("请输入要解析的字符串：")
-	//	fmt.Scanln(&input)
-	//
-	//	output := parseToMarkdown(input)
-	//	fmt.Println(output)
-
-	var stri = "## aaa\n## aaabbb => bbb => ccc\n## ee => fff "
-
-	//var list = []string{"Orange", "Apple", "Banana", "Grape"}
-	//var out = mapStr(list, func(it string) string {
-	//	return it + "==="
-	//})
+	str1, _ := ioutil.ReadFile("/Users/lisp/.zshrc")
+	stri := string(str1)
 	lines := strings.Split(stri, "\n")
 
-	//fmt.Println(lines)
 	for _, line := range lines {
-		outlines := strings.Split(strings.Replace(line, "##", "", 1), "=>")
-		//fmt.Println(len(outline)) // 1, 3, 2
-		for num, outline := range outlines {
-			//fmt.Println(num)
-			//fmt.Println(outline)
-			// := 是类型推导的意思，赋值。
-			oline := strings.TrimSpace(outline)
-			
-			if num == 0 {
-				fmt.Println("- " + oline)
-			} else if num == 1 {
-				fmt.Println("- " + "    " + oline)
-			} else if num == 2 {
-				fmt.Println("- " + "        " + oline)
+		if strings.HasPrefix(line, "##") {
+			outlineRe, _ := regexp.Compile(`=>|=》`)
+			outlines := outlineRe.Split(strings.Replace(line, "##", "", 1), -1)
+			for num, outline := range outlines {
+				// := 是类型推导的意思，赋值。
+				oline := strings.TrimSpace(outline)
+				fmt.Println(repeatString("    ", num) + "- " + oline)
 			}
 		}
 	}
-	// - aaa
-	// - aaabbb
-	// -     bbb
-	// -         ccc
-	// - ee
-	// -     fff
-	// 	
 
 }
-
