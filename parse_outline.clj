b195e53b2f820d8c4d3d55931127eae72aec6b51
stevechan <chanshunli@gmail.com>
2024-03-15 17:27:00 +0800

    WIP

--- parse_outline.clj
+++ parse_outline.clj
@@ -1,17 +1,39 @@
+;; 最先数列中找到通项公式： 从不断列出高阶化中找到前一个和后一个的变换关系， 如 n1 = X * n0， x1 = X * ( X * n0) ...
 (def factorial
   (fn [n]
-    (loop [cnt n
-           acc 1]
-      (if (zero? cnt)
-        acc
-        (recur (dec cnt) (* acc cnt))))))
+    ;; 递归起始条件
+    (loop
+        [cnt n 
+         acc 1]
+        (if (zero? cnt) ;; 递归停止条件是什么
+          acc
+          ;; 递归下去什么
+          (recur
+            (dec cnt)
+            (* acc cnt))))))
 
 ;; 如果下一个比上一个level值更高，那就swap加到它的子集里面，如果小于它就不加进去，同级也不加进去。
-"
-a
- b
-  c
-"
+(def t1
+"a
+    b
+        c
+")
+
+
+(let [text t1]
+  (let [lines (clojure.string/split text #"\n")
+        _ (prn lines)
+        outline (into [] (for [line lines]
+                           (let [indent-level (count (re-find #"(\s){3}" line))
+                                 ;; content (clojure.string/trim (subs line indent-level))
+                                 content (clojure.string/trim line)]
+                             {:indent indent-level
+                              :content content})))
+        formatted-outline {}
+        current-parent nil]
+    outline
+    ))
+
 
 
 (defn parse-outline [text]
